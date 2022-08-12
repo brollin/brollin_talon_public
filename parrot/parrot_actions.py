8833e7a4839fe799bcd73e97addb32512d0e97a4
Ben Rollin <ben.rollin@clever.com>
2022-08-11 19:58:38 -0700

    parrot improvements

--- parrot/parrot_actions.py
+++ parrot/parrot_actions.py
@@ -1,18 +1,20 @@
-from talon import Module, Context, ui, ctrl, canvas, screen, actions, clip
+from talon import Module, Context, ui, ctrl, canvas, screen, actions
 import time
 
 mod = Module()
 mod.list("parrot_sound", desc="List of all parrot sounds")
 
 
-@mod.capture(rule="( <self.any_alphanumeric_key> | click | right click | nothing )")
+@mod.capture(
+    rule="( <self.any_alphanumeric_key> | click | right click | nothing | second )"
+)
 def alphanumeric_or_action(m) -> str:
     "any alphanumeric key"
     return str(m)
 
 
 ctx = Context()
-ctx.lists["user.parrot_sound"] = {"tongue click": "tongue_click", "caveman": "caveman"}
+ctx.lists["user.parrot_sound"] = {"tongue": "tongue_click", "caveman": "caveman"}
 
 sound_to_action = {}
 
@@ -31,6 +33,8 @@ class ParrotActions:
                 ctrl.mouse_click(button=button, down=True)
                 time.sleep(0.1)
                 ctrl.mouse_click(button=button, up=True)
+            elif "second" in action:
+                actions.core.repeat_phrase(1)
 
         print(f"DOING    parrot({sound}): {action}")
 
