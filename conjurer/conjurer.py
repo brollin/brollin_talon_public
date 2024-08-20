c3b6eed255bc523415c56a9fa171a26fac79012e
Ben Rollin <ben.rollin@clever.com>
2024-08-20 03:01:45 -0700

    conjurer actions with args

--- conjurer/conjurer.py
+++ conjurer/conjurer.py
@@ -3,66 +3,32 @@ import json
 from websockets.sync.client import connect
 
 mod = Module()
-# mod.list(
-#     "tracker_target",
-#     desc="List of all tracker targets",
-# )
-# mod.list(
-#     "tracker_day",
-#     desc="List of terms that can select a day",
-# )
+mod.list(
+    "conjurer_action",
+    desc="List of all conjurer actions without arguments",
+)
+mod.list(
+    "conjurer_action_with_args",
+    desc="List of all conjurer actions with arguments",
+)
 
-# target_types = [
-#     {"key": "vitamins", "aliases": [], "type": "bool"},
-#     {"key": "dragonBook", "aliases": ["dragon", "dragon book"], "type": "bool"},
-#     {"key": "craftBook", "aliases": ["craft", "craft book"], "type": "bool"},
-#     {"key": "healthWork", "aliases": ["health"], "type": "bool"},
-#     {"key": "walk", "aliases": [], "type": "bool"},
-#     {"key": "somatics", "aliases": [], "type": "bool"},
-#     {"key": "shower", "aliases": [], "type": "bool"},
-#     {"key": "fruit", "aliases": [], "type": "bool"},
-#     {"key": "journal", "aliases": [], "type": "bool"},
-#     {"key": "mood", "aliases": [], "type": "int"},
-#     {"key": "pain", "aliases": [], "type": "int"},
-#     {"key": "nonStandardSitting", "aliases": ["sitting"], "type": "int"},
-#     {"key": "walking", "aliases": [], "type": "int"},
-#     {"key": "wakeTime", "aliases": ["wake", "wake time"], "type": "int"},
-#     {"key": "sleepTime", "aliases": ["sleep", "sleep time"], "type": "int"},
-#     {
-#         "key": "disruptedTime",
-#         "aliases": ["disrupted", "disrupted time", "disrupt"],
-#         "type": "int",
-#     },
-#     {"key": "notes", "aliases": [], "type": "str"},
-# ]
-
-# target_map = {}
-# for target_type in target_types:
-#     target_map[target_type["key"]] = target_type["key"]
-#     for alias in target_type["aliases"]:
-#         target_map[alias] = target_type["key"]
 
 ctx = Context()
-# ctx.lists["user.tracker_target"] = target_map
-# ctx.lists["user.tracker_day"] = {
-#     "today": "today",
-#     "yesterday": "yesterday",
-#     "tomorrow": "tomorrow",
-#     # "next": "next",
-#     # "last": "last",
-#     "Sunday": "Sunday",
-#     "Monday": "Monday",
-#     "Tuesday": "Tuesday",
-#     "Wednesday": "Wednesday",
-#     "Thursday": "Thursday",
-#     "Friday": "Friday",
-#     "Saturday": "Saturday",
-# }
+ctx.lists["user.conjurer_action"] = {
+    "play": "togglePlay",
+    "pause": "togglePlay",
+    "go to beginning": "goToBeginning",
+    "go to begin": "goToBeginning",
+    "beginning": "goToBeginning",
+    "begin": "goToBeginning",
+    "go to end": "goToEnd",
+}
+ctx.lists["user.conjurer_action_with_args"] = {
+    "layer": "selectLayer",
+}
 
 
-def send_action_message(
-    action: str, *, target: str = "", value: bool | int | str | None = None
-):
+def send_action_message(action: str, *, value: bool | int | str | None = None):
     with connect("ws://127.0.0.1:5174") as websocket:
         # will wait 5 seconds for the server to close the connection normally
         websocket.close_timeout = 0.01
@@ -73,7 +39,6 @@ def send_action_message(
                     "appId": "conjurer",
                     "action": action,
                     "value": value,
-                    "target": target,
                 }
             )
         )
@@ -81,18 +46,16 @@ def send_action_message(
 
 @mod.action_class
 class ConjurerActions:
-    def conjurer_send_action_with_args(
-        action: str, target: str, value_type: str, value: int | str
-    ):
+    def conjurer_send_action_with_args(action: str, value_type: str, value: int | str):
         """Send action message with arguments to conjurer app"""
         if value_type == "int":
-            send_action_message(action, target=target, value=int(value))
+            send_action_message(action, value=int(value))
         elif value_type == "str":
-            send_action_message(action, target=target, value=str(value))
+            send_action_message(action, value=str(value))
         elif value_type == "bool":
-            send_action_message(action, target=target, value=True if value else False)
+            send_action_message(action, value=True if value else False)
 
-    def conjurer_send_action(action: str, value: str):
+    def conjurer_send_action(action: str):
         """Send action message to conjurer app"""
-        app.notify(f"action: {action}, value: {value}")
-        send_action_message(action, value=value)
+        app.notify(f"action: {action}")
+        send_action_message(action)
--- conjurer/conjurer.talon
+++ conjurer/conjurer.talon
@@ -2,9 +2,5 @@ tag: browser
 browser.host: localhost:3000
 -
 
-# track {user.conjurer_target}: user.conjurer_send_action_with_args("track", conjurer_target, "bool", 1)
-# untrack {user.conjurer_target}: user.conjurer_send_action_with_args("track", conjurer_target, "bool", 0)
-
-# track {user.conjurer_target} <number>: user.conjurer_send_action_with_args("track", conjurer_target, "int", number)
-
-testing: user.conjurer_send_action("action", "testing")
+{user.conjurer_action}: user.conjurer_send_action(conjurer_action)
+{user.conjurer_action_with_args} <number>: user.conjurer_send_action_with_args(conjurer_action_with_args, "int", number)
